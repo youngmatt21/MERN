6ffa15b5e3343b84f8a0a91049902a1dd6cafe98
dizas9 <sazid.ice07@gmail.com>
2024-02-23 16:07:55 +0600

    Server Update cors origin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://mern-front-blush.vercel.app",
+    origin: "https://mern-vihv.onrender.com",
     credentials: true,
   })
 );
